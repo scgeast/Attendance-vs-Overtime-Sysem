0c9166c770f168e3ff80eea8202d9577efd804eb
scgeast <scgeast@gmail.com>
2025-10-29 22:39:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.markdown(
     """
     <style>
     .block-container {
-        padding-top: 2rem !important;
+        padding-top: 1.5rem !important;
         padding-bottom: 0rem !important;
         padding-left: 1rem !important;
         padding-right: 1rem !important;
