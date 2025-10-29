4395c4f97381701e5b686002271b4b31a997437d
scgeast <scgeast@gmail.com>
2025-10-30 00:05:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -634,14 +634,14 @@ if uploaded_overtime is not None and uploaded_rekap is not None:
                     st.dataframe(
                         styled_df,
                         use_container_width=True,
-                        height=1200  # 🚀 Diperbesar agar lebih luas
+                        height=600  # 🚀 Diperbesar agar lebih luas
                     )
                 except:
                     # Fallback jika styling error
                     st.dataframe(
                         display_df,
                         use_container_width=True,
-                        height=1200  # 🚀 Diperbesar agar lebih luas
+                        height=600  # 🚀 Diperbesar agar lebih luas
                     )
             
             with tab2:
